f21e5021faf5c2262bd2beb9d05acdf7654798be
anzajac <a_zajac1@massbay.edu>
2019-10-04 09:08:43 -0400

    Added files

--- src/RecursiveOnion.java
+++ src/RecursiveOnion.java
@@ -1,4 +1,60 @@
 
 public class RecursiveOnion {
-
+	private String message;
+	
+	public RecursiveOnion(){
+		message = "0";
+		setMessage(recursiveReverse(message));
+		System.out.println(toString());
+	}
+	
+	public RecursiveOnion(String m) {
+		setMessage(recursiveReverse(m));
+		System.out.println(toString());
+	}
+	
+	public String toString() {
+		return message;
+	}
+	
+	public boolean equals(Object obj) {
+		if(this == obj) {
+			return true;
+		}
+		if(null == obj) {
+			return false;
+		}
+		if(obj instanceof RecursiveOnion) {
+			RecursiveOnion onion = (RecursiveOnion) obj;
+			return message.equals(onion.message);
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public void setMessage(String m) {
+		message = m;
+	}
+	
+	private String reverseDigit(char d) {
+		if(d == '0') {
+			return "1";
+		}
+		else {
+			return "0";
+		}
+	}
+	
+	private String recursiveReverse(String m) {
+		int len = message.length();
+		String innerMessage;
+		if(len == 1) {
+			return reverseDigit(message.charAt(0));
+		}
+		else {
+			innerMessage = recursiveReverse(message.substring(1, len - 1));
+			return reverseDigit(message.charAt(0)) + innerMessage + reverseDigit(message.charAt(len - 1));
+		}
+	}
 }
--- src/RecursiveOnionTester.java
+++ src/RecursiveOnionTester.java
@@ -1,4 +1,26 @@
-
+import java.util.Scanner;
 public class RecursiveOnionTester {
-
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String message;
+		try {
+			System.out.println("Enter an odd number of binary digits: ");
+			message = scan.next();
+			if(message.length() % 2 == 0) {
+				scan.close();
+				Exception e1 = new Exception("This is an even number of binary digits. Unacceptable!");
+				throw e1;
+			}
+			for(int i = 0; i < message.length(); i++) {
+				if(message.charAt(i) < 0 || message.charAt(i) > 1) {
+					scan.close();
+					Exception e2 = new Exception("A digit is not binary. Unacceptable!");
+					throw e2;
+				}
+			}
+		}
+		catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
 }
