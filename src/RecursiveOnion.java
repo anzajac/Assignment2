484b9acfcf794640821a6c144685b2a5a05f9422
anzajac <a_zajac1@massbay.edu>
2019-10-11 10:03:33 -0400

    Typed in comment documentation

--- src/RecursiveOnion.java
+++ src/RecursiveOnion.java
@@ -1,24 +1,41 @@
-//Assignment 2
-//Stephen Shea and Abigail Zajac
-//Flips the digits of a binary number
+/*Assignment Two
+ * RecursiveOnion.java
+ * Stephen Shea and Abigail Zajac
+ * 10-11-19
+ * CS208
+ * 
+ * This class contains a recursive method that takes a binary number which is secret & decodes to its flipped version
+ * meaning all binary digits switched from 0 to 1 & vice versa
+ * Also a helper method called reverseDigit() helps recursive method to flip the digits of a binary number
+ * Otherwise the class has the regular initiation of toString(), equals(), getter & setter methods
+ */
 public class RecursiveOnion {
-	//Attributes
+	//class attributes
 	private String message;
+	private String secret;
 	
-	//Constructors
+	//class constructors
+	//default
 	public RecursiveOnion(){
 		message = "0";
+		secret = "0";
 		setMessage(recursiveReverse(message));
 	}
+	//with parameter
 	public RecursiveOnion(String m) {
+		secret = m;
 		setMessage(recursiveReverse(m));
 	}
 	
 	
 	//toString and equals methods
+	//toString method to return formatted info on each RecursiveOnion
+	@Override
 	public String toString() {
-		return message;
+		return "Encryted message (" + secret + ") is decryted as " + message;
 	}
+	//equals method to ensure inputed object is RecursiveOnion object
+	@Override
 	public boolean equals(Object obj) {
 		if(this == obj) {
 			return true;
@@ -28,7 +45,8 @@ public class RecursiveOnion {
 		}
 		if(obj instanceof RecursiveOnion) {
 			RecursiveOnion onion = (RecursiveOnion) obj;
-			return message.equals(onion.message);
+			return message.equals(onion.message) && 
+					secret.equals(onion.secret);
 		}
 		else {
 			return false;
@@ -38,6 +56,10 @@ public class RecursiveOnion {
 	//getter and setter methods
 	public void setMessage(String m) {
 		message = m;
+	}
+	
+	public String getSecret() {
+		return secret;
 	}
 	
 	//helper method
@@ -50,7 +72,7 @@ public class RecursiveOnion {
 		}
 	}
 	
-	//recursive helper method for constructor
+	//recursive method for constructor
 	private String recursiveReverse(String m) {
 		int len = m.length();
 		String innerMessage;
--- src/RecursiveOnionTester.java
+++ src/RecursiveOnionTester.java
@@ -1,25 +1,42 @@
-//Assignment 2
-//Stephen Shea and Abigail Zajac
+/*Assignment Two
+ * RecursiveOnionTester.java
+ * Stephen Shea and Abigail Zajac
+ * 10-11-19
+ * CS208
+ * 
+ * This class tests recursion from RecursiveOnion
+ * User must input secret binary number in odd number of digits in 0s & 1s
+ * Program must print out the decoded & flipped binary number
+ * This goes on continuously in a while loop until user enters "exit"
+ */
 import java.util.Scanner;
 public class RecursiveOnionTester {
 	public static void main(String[] args) {
+		//initiate variable and scanner for user input
 		Scanner scan = new Scanner(System.in);
 		String message;
+		//flag to continue while loop as false & true for exit
 		boolean exitFlag = false;
+		//while loop to repeat the recursive class
 		while(!exitFlag) {
+			//looking for errors as even number of binary digits or digits not 0 and/or 1
 			try {
-				System.out.println("Enter an odd number of binary digits or exit to end program: ");
+				//asks user input
+				System.out.println("Enter an odd number of binary digits to continue or 'exit' to end program: ");
 				message = scan.next();
+				//changes flag to true to end loop when user enters "exit"
 				if(message.equals("exit")) {
 					exitFlag = true;
 					scan.close();
 				}
 				else{
+					//checking for any even amount of binary digits which prints exception
 					if(message.length() % 2 == 0) {
 						scan.close();
 						Exception e1 = new Exception("This is an even number of digits. Unacceptable!");
 						throw e1;
 					}
+					//checking for any non-binary digit which prints exception
 					for(int i = 0; i < message.length(); i++) {
 						if(message.charAt(i) != '0' && message.charAt(i) != '1') {
 							scan.close();
@@ -27,10 +44,13 @@ public class RecursiveOnionTester {
 							throw e2;
 						}
 					}
+					//if no exceptions then create RecursiveOnion object
 					RecursiveOnion onion1 = new RecursiveOnion(message);
+					//outputs decoded & flipped binary number
 					System.out.println(onion1.toString());
 				}
 			}
+			//will catch any exception to then print out exception message
 			catch(Exception e) {
 				System.out.println(e.getMessage());
 				exitFlag = true;
